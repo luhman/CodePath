b4f25b5f89f785be13a1fcc49ec83c50ac2bcfbb
David Luhman <david.luhman@gmail.com>
2014-06-25 17:45:46 -0700

    Vertical Pan Works

--- Week 3 - Animation and Gestures/Paper Animation/Paper Animation/headlinesViewController.m
+++ Week 3 - Animation and Gestures/Paper Animation/Paper Animation/headlinesViewController.m
@@ -9,6 +9,12 @@
 #import "headlinesViewController.h"
 
 @interface headlinesViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *headlinesImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *trendingImageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *trendingScrollview;
+@property (nonatomic, assign) CGPoint originalState;
+@property (weak, nonatomic) IBOutlet UIView *mainView;
+
 
 @end
 
@@ -27,6 +33,14 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    // Initialize Pan Gesture Recognizer
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onCustomPan:)];
+    [self.view addGestureRecognizer:panGestureRecognizer];
+    [self.view setUserInteractionEnabled:YES];
+    
+    // Initialize Scroll View
+    [self.trendingScrollview setScrollEnabled:YES];
+    [self.trendingScrollview setContentSize:CGSizeMake(1444, 253)];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +48,73 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+// Pan action
+- (void)onCustomPan:(UIPanGestureRecognizer *)panGestureRecognizer {
+    CGPoint point = [panGestureRecognizer locationInView:self.view];
+    CGPoint velocity = [panGestureRecognizer velocityInView:self.view];
+    
+    // Calculate offset
+    int difference = 0;
+    
+    if (panGestureRecognizer.state == UIGestureRecognizerStateBegan) {
+        self.originalState = point;
+    }
+    else if (panGestureRecognizer.state == UIGestureRecognizerStateChanged) {
+        difference = self.originalState.y - point.y;
+        self.originalState = point;
+    }
+    else if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        difference = self.originalState.y - point.y;
+    }
+    
+    // Set Offset
+    self.mainView.center = CGPointMake(self.mainView.center.x, self.mainView.center.y - difference);
+    
+    // You hit the ceiling
+    if (self.mainView.center.y < 284) {
+        
+        if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+            
+            [UIView animateWithDuration: 2
+                                  delay: 0
+                 usingSpringWithDamping: .9
+                  initialSpringVelocity: 5
+                                options: 0
+                             animations: ^
+             {
+                 self.mainView.center = CGPointMake(160, 284);
+             }
+                             completion: nil
+             ];
+            
+        }
+        else if(panGestureRecognizer.state == UIGestureRecognizerStateChanged && self.mainView.center.y < 244) {
+            self.mainView.center = CGPointMake(160, 244);
+        }
+    }
+    
+    else if(self.mainView.center.y == 852) {
+        self.mainView.center = CGPointMake(160, 812);
+    }
+    
+    
+    // Resting place
+    if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        if (self.mainView.center.y > 300 && velocity.y > 10) {
+            [UIView animateWithDuration:.5 animations:^{
+                self.mainView.center = CGPointMake(160, 812);
+            }];
+        }
+        
+        // Come back up here
+        else if(self.mainView.center.y < 800 && velocity.y < -10) {
+            [UIView animateWithDuration:.5 animations:^{
+                self.mainView.center = CGPointMake(160, 284);
+            }];
+        }
+    }
+}
+
 
 @end
