d02da7456a643e3a29dbe1b1c41f62c301ee00c7
David Luhman <david.luhman@gmail.com>
2014-06-25 17:22:49 -0700

    Start Fresh Setup

---
+++ Week 3 - Animation and Gestures/Paper Animation/Paper Animation/headlinesViewController.h
@@ -0,0 +1,13 @@
+//
+//  headlinesViewController.h
+//  Paper Animation
+//
+//  Created by David Luhman on 6/25/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface headlinesViewController : UIViewController
+
+@end
